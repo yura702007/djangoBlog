676c9b444d8ac230b520d4dcd535229081de763a
yura702007 <yura702007@gmail.com>
2023-11-29 01:04:22 +0300

    add except Http404 in function view post_detail

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 
 
 def post_list(request):
     post = Post.published.all()
     return render(request, 'blog/post/list.html', {'post': post})
+
+
+def post_detail(request, post_id):
+    try:
+        post = Post.published.get(id=post_id)
+    except Post.DoesNotExist:
+        raise Http404('No Post Found')
+
+    return render(request, 'blog/post/detail.html', {'post': post})
