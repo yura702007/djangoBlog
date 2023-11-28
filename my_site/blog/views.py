4eae40f5de83880a05420ff78399e9f59d5f2148
yura702007 <yura702007@gmail.com>
2023-11-29 00:58:28 +0300

    create function view post_list

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    post = Post.published.all()
+    return render(request, 'blog/post/list.html', {'post': post})
