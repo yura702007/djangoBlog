8850d1efca4672d199c8831528a9a8f1a3ef364c
yura702007 <yura702007@gmail.com>
2023-12-02 14:44:37 +0300

    change path to view function 'post_detail'

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     #  представления поста
     path('', views.post_list, name='post_list'),
-    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
