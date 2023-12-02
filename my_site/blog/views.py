8c4e11b66f1cadf50ed2b1ec6a7b53ad299e2201
yura702007 <yura702007@gmail.com>
2023-12-02 16:17:31 +0300

    post_list >> posts in body function 'post_list'

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -5,9 +5,9 @@ from django.core.paginator import Paginator
 
 
 def post_list(request):
-    post_list = Post.published.all()
+    posts = Post.published.all()
     # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 3)
+    paginator = Paginator(posts, 3)
     page_number = request.GET.get('page', 1)
     posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
