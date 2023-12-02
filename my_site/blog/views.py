46070be61a88069007da15158dd56729cf4dd260
yura702007 <yura702007@gmail.com>
2023-12-02 16:04:48 +0300

    create paginator in function view 'post_list'

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    post = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': post})
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, slug):
