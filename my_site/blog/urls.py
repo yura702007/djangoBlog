9f0b487be2225ee687e445fadfa084b7a34730d6
yura702007 <yura702007@gmail.com>
2023-12-02 15:25:06 +0300

    bag fix

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     #  представления поста
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -8,10 +8,10 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': post})
 
 
-def post_detail(request, year, month, day, post):
+def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post,
                              status=Post.Status.PUBLISHED,
-                             slug=post,
+                             slug=slug,
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
