99e28cedbe3c5e5bbb617a29b6afcff23c30a3de
yura702007 <yura702007@gmail.com>
2023-12-02 15:21:21 +0300

    update method 'get_absolute_url' in model Post

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -34,7 +34,10 @@ class Post(models.Model):
         ]
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
     def __str__(self):
         return self.title
