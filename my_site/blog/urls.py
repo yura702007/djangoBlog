b0811500272abdbfd52310ea2a75aae990b036fc
yura702007 <yura702007@gmail.com>
2023-12-03 16:55:09 +0300

    bag fix

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     #  представления поста
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<int:post_id/share/>', views.post_share, name='post_share'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
