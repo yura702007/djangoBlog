0d97415dbef3f917da7a08396ac1bbb2389ae972
yura702007 <yura702007@gmail.com>
2023-12-03 12:16:49 +0300

    edit urls.py for views by class

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     #  представления поста
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
