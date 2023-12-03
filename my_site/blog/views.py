98136cc81f76193fd39fc52c20a7ab368ee838f0
yura702007 <yura702007@gmail.com>
2023-12-03 12:13:00 +0300

    use ListView template

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    posts = Post.published.all()
-    # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число,
-        # выдать 1 страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона,
-        # то выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    """
+    Представление списка постов на основе класса
+    """
+    queryset = Post.publish.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, slug):
