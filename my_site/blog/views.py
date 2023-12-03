656c0e5021aefcaa00d7d64f7460418ad7c3e509
yura702007 <yura702007@gmail.com>
2023-12-03 16:39:09 +0300

    send email in view

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -18,16 +19,21 @@ class PostListView(ListView):
 def post_share(request, post_id):
     #  Извлечь пост по индетификатору id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         # Форма была передана на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-            # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommend you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'yura702007@gmail.com', cd['to'])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
 def post_detail(request, year, month, day, slug):
