906f9a5634248ffb8c5242cfbfe60c71fb41fcb7
yura702007 <yura702007@gmail.com>
2023-12-06 16:49:38 +0300

    add view function 'post_comment'

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,9 +1,26 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.http', {'post': post, 'form': form, 'comment': comment})
 
 
 class PostListView(ListView):
