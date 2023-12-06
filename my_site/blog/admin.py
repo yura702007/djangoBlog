1f5e7e12669284f201bbdd7db0567116885909b6
yura702007 <yura702007@gmail.com>
2023-12-06 16:26:26 +0300

    add model Comment to page Admin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -12,4 +12,9 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'update']
+    search_fields = ['name', 'email', 'body']
