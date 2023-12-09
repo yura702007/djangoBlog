0ef4bdfa2b4d8bf78fe4d7e846d292b5072f4219
yura702007 <yura702007@gmail.com>
2023-12-09 11:32:31 +0300

    creating a template include tag

--- my_site/blog/templatetags/blog_tags.py
+++ my_site/blog/templatetags/blog_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_posts(count=5):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post': latest_post}
