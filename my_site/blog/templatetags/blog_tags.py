b74998b3cc3de82e18d5558941fba51438b5f963
yura702007 <yura702007@gmail.com>
2023-12-09 11:36:45 +0300

    latest_post >> latest_posts

--- my_site/blog/templatetags/blog_tags.py
+++ my_site/blog/templatetags/blog_tags.py
@@ -9,7 +9,7 @@ def total_posts():
     return Post.published.count()
 
 
-@register.inclusion_tag('blog/post/latest_post.html')
+@register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
-    latest_post = Post.published.order_by('-publish')[:count]
-    return {'latest_post': latest_post}
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
