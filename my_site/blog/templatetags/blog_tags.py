893c7791b5751c25510bf8c34a9010572f1e1a0d
yura702007 <yura702007@gmail.com>
2023-12-09 12:41:14 +0300

    create a template filter to support syntax Markdown

--- my_site/blog/templatetags/blog_tags.py
+++ my_site/blog/templatetags/blog_tags.py
@@ -1,8 +1,15 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 from ..models import Post
+import markdown
 
 register = template.Library()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 @register.simple_tag
