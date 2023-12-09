0e8740ced7694cc726fd2fa3c4b0e4308a4f3729
yura702007 <yura702007@gmail.com>
2023-12-09 13:57:43 +0300

    bag fix

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -18,7 +18,7 @@ import django.contrib.sitemaps.views
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from ..blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap
 
 
 sitemaps = {
