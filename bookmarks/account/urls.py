e4b73db6c1b300b43026a4ce6bd4bc6df83f451d
yura702007 <yura702007@gmail.com>
2023-12-16 17:04:01 +0300

    add path to reset password in urlpatterns

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -3,10 +3,36 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    # path('login/', views.user_login, name='login'),
+    # url адреса входа и выхода
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # url адреса смены пароля
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # url адреса сброса пароля
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'password_reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password_reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
+
+    path('', views.dashboard, name='dashboard'),
+    # path('login/', views.user_login, name='login'),
 ]
