98f662f291e460321f744b9c7854042c90a03779
yura702007 <yura702007@gmail.com>
2023-12-16 15:39:51 +0300

    add path to pages html for password change and password change done

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -3,8 +3,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
