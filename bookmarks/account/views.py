8cf645c4f8064306aaf56f4fbaeaac8708e9cf8f
yura702007 <yura702007@gmail.com>
2023-12-17 18:40:05 +0300

    add view function for registration new users

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
+            new_user = user_form.save(commit=False)
+            # Установить выбранный пароль
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохранить объект User
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
 @login_required
