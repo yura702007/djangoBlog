eff2eee15a4935f82664e572b341def1759c5a5d
yura702007 <yura702007@gmail.com>
2023-12-17 18:27:46 +0300

    add password == password2 check

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -14,3 +14,9 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Password don\'t match.')
+        return cd['password2']
