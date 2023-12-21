cc425d54f38cdd7ec0d976a50f0815c2da9d9bbb
yura702007 <yura702007@gmail.com>
2023-12-21 17:31:04 +0300

    add name_dir in path to register.html template

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -16,10 +16,10 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Сохранить объект User
             new_user.save()
-            return render(request, 'account/register_done.html', {'new_user': new_user})
+            return render(request, 'account/template/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form': user_form})
+    return render(request, 'account/template/register.html', {'user_form': user_form})
 
 
 @login_required
