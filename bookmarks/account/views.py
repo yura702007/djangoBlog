b9cdb048fa24a600c4ae3eac19250122305dbd75
yura702007 <yura702007@gmail.com>
2023-12-23 17:20:54 +0300

    add view function edit profile form

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
 
 
 def register(request):
@@ -16,6 +31,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Сохранить объект User
             new_user.save()
+            # Создать профиль пользователя
+            Profile.objects.create(user=new_user)
             return render(request, 'account/template/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
