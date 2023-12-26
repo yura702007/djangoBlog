55dc660b0f5cde75283ab4c2e7ea39abecd98d45
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-12-26 13:54:53 +0300

    add check existing email for auth

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -8,6 +8,13 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'email']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exist():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
@@ -33,3 +40,9 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Password don\'t match.')
         return cd['password2']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exist():
+            raise forms.ValidationError('Email already in use.')
+        return data
